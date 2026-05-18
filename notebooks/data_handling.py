72e3e41ef64fac3489a2c035d240187b9d17ed5b
dina-masry <dina40012@gmail.com>
2026-05-18 15:45:32 +0300

    The first part of the hidden message : Data opens doors

--- notebooks/data_handling.py
+++ notebooks/data_handling.py
@@ -29,4 +29,4 @@ print(df_filtered.head())
 df_filtered['message'] = df_filtered['last_name'].apply(
     lambda x: f"The first is {x[0]}"
 )
-print(df_filtered[['last_name', 'message']].head())
+print(df_filtered[['last_name', 'message']])
